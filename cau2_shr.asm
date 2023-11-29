8cb126f04287a1781b17ae511bcad24f0b44b2f1
Duy Dat <phamduydat2002@gmail.com>
2023-11-29 22:55:44 +0700

    up

--- cau2_shr.asm
+++ cau2_shr.asm
@@ -2,62 +2,59 @@ INCLUDE LIB1.ASM
 .MODEL small
 .STACK 100h
 .DATA
-  tieuDe   db 13, 10, 'TRUNG BINH CONG HAI SO NGUYEN'
-           db 13, 10, '-------------------------------$'
-  nhapSo1  db 13, 10, 'Hay vao so thu nhat: $'
-  nhapSo2  db 13, 10, 'Hay vao so thu hai: $'
-  ketQua   db 13, 10, 'TBC hai so nguyen la: $'
-  phay     db '.5$'
-  mTiepTuc db 13,10,'Co tiep tuc chuong trinh (c/k)? $'
-  sum      dw ?
+        tieuDe   db 13, 10, 'TRUNG BINH CONG HAI SO NGUYEN'
+                 db 13, 10, '-------------------------------$'
+        nhapSo1  db 13, 10, 'Hay vao so thu nhat: $'
+        nhapSo2  db 13, 10, 'Hay vao so thu hai: $'
+        ketQua   db 13, 10, 'TBC hai so nguyen la: $'
+        dauTru   db '-$'
+        phay     db '.5$'
+        mTiepTuc db 13,10,'Co tiep tuc chuong trinh (c/k)? $'
 .CODE
-  MAIN:   
-          mov        ax, @data
-          mov        ds, ax
+        MAIN:   
+                mov        ax, @data
+                mov        ds, ax
 
-          CLRSCR
-          HienString tieuDe
+                CLRSCR
+                HienString tieuDe
 
-          HienString nhapSo1
-          call       VAO_SO_N
-          mov        bx, ax
+                HienString nhapSo1
+                call       VAO_SO_N
+                mov        bx, ax
 
-          HienString nhapSo2
-          call       VAO_SO_N
+                HienString nhapSo2
+                call       VAO_SO_N
+                
+                HienString ketQua
           
-          xor        dx, dx
-          add        ax, bx
-          mov        sum, ax
+                add        ax, bx
 
-          cmp        sum, 0
-          jge        L1
-          neg        ax
-  L1:     
-          mov        bx, 2
-          div        bx
+                cmp        ax, 0
+                jge        DUONG
+                neg        ax
+                HienString dauTru
 
-          HienString ketQua
-          cmp        sum, 0
-          jge        L2
-          neg        ax
-  L2:     
-          call       HIEN_SO_N
+        DUONG:  
+                shr        ax, 1
+                pushf
 
-          cmp        dx, 0
-          je         TiepTuc
-          HienString phay
+                call       HIEN_SO_N
 
-  TiepTuc:
-          HienString mTiepTuc
-          mov        ah, 01h
-          int        21h
-          cmp        al,'c'
-          jne        Exit
-          jmp        MAIN
+                popf
+                jnc        TiepTuc
+                HienString phay
 
-  Exit:   
-          mov        ah, 4ch
-          int        21h
-          INCLUDE    LIB2.ASM
-          INCLUDE    LIB3.ASM
+        TiepTuc:
+                HienString mTiepTuc
+                mov        ah, 01h
+                int        21h
+                cmp        al,'c'
+                jne        Exit
+                jmp        MAIN
+
+        Exit:   
+                mov        ah, 4ch
+                int        21h
+                INCLUDE    LIB2.ASM
+                INCLUDE    LIB3.ASM
 END MAIN
